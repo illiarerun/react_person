a9373030bbd9785f379de0522396297c43069c78
Illia Vovchenko <ilya.vovchenko@gmail.com>
2021-08-21 16:47:42 +0300

    Updates

---
+++ precache-manifest.67ebad7f45ccb1bd38b91133f07837cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "549dea9868fb3c11a1f85214d1c8258d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2d7c58ce437da31002f5",
+    "url": "./static/css/main.835580e9.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d7c58ce437da31002f5",
+    "url": "./static/js/main.40ba8f4f.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
